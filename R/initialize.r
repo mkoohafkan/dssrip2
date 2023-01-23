a724b5bd8d562b32c5f9cfd6580dec6d4d8ea4f0
Michael C Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-01-23 08:03:11 -0800

    scaffolding for monolith
    and improved selection of jar dependencies

--- R/initialize.r
+++ R/initialize.r
@@ -19,10 +19,11 @@ dss_message_level = function(message_level) {
       "Check that DSS connection is working.", call. = FALSE))
 }
 
+
 #' @rdname dss_connect
 #' @export
 dss_require = function(dss_home = getOption("dss.home"),
-  message_level = getOption("dss.messagelevel")) {
+  message_level = getOption("dss.messagelevel"), monolith = TRUE) {
   result = try(dss_connect(dss_home))
   if (inherits(result, "try-error")) {
     invisible(FALSE)
@@ -30,6 +31,7 @@ dss_require = function(dss_home = getOption("dss.home"),
     invisible(TRUE)
   }
 }
+
 
 #' Connect To DSS
 #'
@@ -47,11 +49,12 @@ dss_require = function(dss_home = getOption("dss.home"),
 #' @importFrom rJava .jnew J
 #' @export
 dss_connect = function(dss_home = getOption("dss.home"),
-  message_level = getOption("dss.messagelevel")) {
+  message_level = getOption("dss.messagelevel"),
+  monolith = getOption("dss.monolith")) {
   if (.Platform$OS.type == "windows") {
-    dss_connect_win(dss_home, message_level)
+    dss_connect_win(dss_home, message_level, isTRUE(monolith))
   } else {
-    dss_connect_unix(dss_home, message_level)
+    dss_connect_unix(dss_home, message_level, isTRUE(monolith))
   }
   assign("hec.io.TimeSeriesContainer",
     java_fields(.jnew("hec/io/TimeSeriesContainer")), hecJavaObjectsDB)
@@ -65,18 +68,16 @@ dss_connect = function(dss_home = getOption("dss.home"),
 }
 
 
+#' @rdname dss_connect
 #' @importFrom rJava javaImport .jaddLibrary .jaddClassPath
 #'   .jcall
-#' @name dss_connect
-dss_connect_win = function(dss_home, message_level) {
-  # set Java home to DSS install
-#  Sys.setenv(JAVA_HOME = normalizePath(file.path(dss_home,
-#    "java"), mustWork = TRUE))
+dss_connect_win = function(dss_home, message_level, monolith) {
+
   hec_lib_path = normalizePath(file.path(dss_home,
     "lib/javaHeclib.dll"), mustWork = TRUE)
 
   # initialize JVM
-  jar_paths = get_jars(dss_home)
+  jar_paths = get_jars(dss_home, monolith)
   javaImport(packages = "java.lang")
   .jaddLibrary("javaHeclib", hec_lib_path)
   .jaddClassPath(jar_paths)
@@ -85,8 +86,8 @@ dss_connect_win = function(dss_home, message_level) {
 
 #' @rdname dss_connect
 #' @keywords internal
-dss_connect_unix = function(dss_home, message_level) {
-  stop("Linux not currently supported.")
+dss_connect_unix = function(dss_home, message_level, monolith) {
+  stop("Linux/MacOS is not currently supported.")
 }
 
 
@@ -97,16 +98,32 @@ dss_connect_unix = function(dss_home, message_level) {
 #' @param dss_home The path to the DSS install.
 #' @return A list of paths to required jars.
 #'
+#' @importFrom utils head
 #' @keywords internal
-get_jars = function(dss_home) {
-  required_jars = c("hec.jar", "rma.jar", "lookup.jar",
-    "hec-dssvue.+.jar", "help/dssvueHelp.jar")
+get_jars = function(dss_home, monolith) {
+  if (monolith) {
+    required_jars = c("hec-monolith(?!-compat).*.jar", "hecnf.*.jar",
+      "hec-monolith-compat.*.jar", "hec-nucleus-metadata.*.jar")
+  } else {
+    required_jars = c("hec.jar", "rma.jar", "lookup.jar",
+      "hec-dssvue.+.jar", "help/dssvueHelp.jar")
+  }
   all_jars = dir(normalizePath(file.path(dss_home, "jar"),
     mustWork = TRUE), pattern = ".jar$", recursive = TRUE)
-  selected_jars = all_jars[grep(paste(sprintf("(%s)", required_jars),
-    collapse = "|"), all_jars)]
-  if (length(selected_jars) != length(required_jars)) {
-    warning("jar weirdness, improve this error message")
+  # search for each jar individually so problems can be identified
+  jar_indexes = lapply(sprintf("(%s)", required_jars), grep,
+    all_jars, perl = T)
+  selected_jars = lapply(jar_indexes, function(x)
+    all_jars[x])
+  num_found = sapply(selected_jars, length)
+  if (any(num_found > 1L)) {
+    warning("Multiple matching jars found:\n",
+      paste("\t", sapply(selected_jars[num_found > 1L], paste,
+        collapse = ", "), collapse = "\n"), "\n")
+    selected_jars = lapply(selected_jars, head, 1L)
+  } else if (any(num_found < 1L)) {
+    stop("No matching jars found:\n",
+      paste("\t", required_jars[num_found < 1L], collapse = "\n"))
   }
   normalizePath(file.path(dss_home, "jar", selected_jars),
     mustWork = TRUE)
@@ -166,6 +183,7 @@ metafield = function(jObject, shortname, signature) {
     val
   }
 }
+
 
 #' Get Java Metadata
 #'
