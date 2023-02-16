ad9528ecb54030b4db269091fe22cd55a27f14ee
Michael C Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-02-16 09:45:59 -0800

    fix DSS constants
    - `TSC_INTERVALS` is complete, explicit list
    - `DSS_MISSING_VALUE` is defined to correct number of digits

--- R/constants.r
+++ R/constants.r
@@ -1,23 +1,17 @@
 TSC_TYPES = c("INST-VAL", "INST-CUM", "PER-AVER", "PER-CUM")
 
-minutes = c(1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30)
-
-hours = c(1, 2, 3, 4, 6, 8, 12)
-
-#' @importFrom stats setNames
-TSC_INTERVALS = setNames(
-  c(minutes, 60 * hours, 60 * 24 * c(1, 7, 10, 15, 30, 365), rep(0, 5)),
-## Irregular appears to have interval of 0, not -1
-  c(paste0(minutes, "MIN"), paste0(hours, "HOUR"),
-    "1DAY", "1WEEK", "TRI-MONTH", "SEMI-MONTH", "1MON", "1YEAR",
-    paste0("IR-", c("DAY", "MON", "YEAR", "DECADE", "CENTURY"))))
+TSC_INTERVALS = c("1MIN", "2MIN", "3MIN", "4MIN", "5MIN", "6MIN",
+  "10MIN", "12MIN", "15MIN", "20MIN", "30MIN", "1HOUR", "2HOUR",
+  "3HOUR", "4HOUR", "6HOUR", "8HOUR", "12HOUR", "1DAY", "1WEEK",
+  "1MON", "SEMI-MONTH", "TRI-MONTH", "1YEAR", "IR-DAY", "IR-MON",
+  "IR-YEAR", "IR-DECADE", "IR-CENTURY")
 
 # used to help with introspection on Java Objects
 sigConversions = list(boolean = "Z", byte = "B", char = "C",
   short = "T", void = "V", int = "I", long = "J", float = "F",
   double = "D")
 
-DSS_MISSING_VALUE = -3.402823e+38
+DSS_MISSING_VALUE = -3.402823466385288603362e+38
 
 DSS_PARTS = LETTERS[1:6]
 
