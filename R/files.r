cb5c8d6518b2083f2a67692a1340c743ba7bbad6
Michael C Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-03-02 08:18:03 -0800

    new functions and tests for DSS version handling

--- R/files.r
+++ R/files.r
@@ -7,9 +7,9 @@
 #'
 #' @importFrom rJava .jcall
 #' @keywords internal
-dss_file = function(filename) {
+dss_file = function(filename, ...) {
   file = .jcall("hec/heclib/dss/HecDss", "Lhec/heclib/dss/HecDss;",
-    method = "open", filename)
+    method = "open", filename, ...)
   on.exit(file$done(), add = TRUE)
   file
 }
@@ -27,6 +27,8 @@ dss_file = function(filename) {
 #'   with a message that `javaHeclib.dll` cannot be found, this usually
 #'   indicates that `dss.home` is not set correctly.
 #'
+#' @seealso [dss_close()] [dss_version()]
+#'
 #' @export
 dss_open = function(filename) {
   assert_dss_connected()
@@ -37,14 +39,19 @@ dss_open = function(filename) {
 
 #' @rdname dss_open
 #' @export
-dss_create = function(filename) {
+dss_create = function(filename, version = 7L) {
   assert_dss_connected()
+  version = as.integer(version[1])
+  if (!any(version == c(6L, 7L))) {
+    stop("Only DSS version 6 or 7 is supported")
+  }
   filepath = normalizePath(filename, mustWork = FALSE)
   if (file.exists(filepath)) {
     stop("File ", filepath, " already exists.")
   }
-  dss_file(filepath)
+  dss_file(filepath, version)
 }
+
 
 #' Squeeze a DSS File
 #'
@@ -53,11 +60,74 @@ dss_create = function(filename) {
 #'
 #' @param file A DSS file handle, e.g., output of [dss_open()].
 #'
+#' @seealso [dss_delete()]
+#'
 #' @export
 dss_squeeze = function(file) {
   assert_dss_connected()
   assert_dss_file(file)
   on.exit(file$done(), add = TRUE)
   file$getDataManager()$squeeze()
+  invisible(TRUE)
+}
+
+
+#' DSS File Version
+#'
+#' Get the DSS file version.
+#'
+#' @inheritParams dss_squeeze
+#' @return An integer
+#'
+#' @seealso [dss_convert()]
+#'
+#' @export
+dss_version = function(file) {
+  assert_dss_connected()
+  assert_dss_file(file)
+  on.exit(file$done(), add = TRUE)
+  as.integer(file$getDataManager()$getDssFileVersion())
+}
+
+
+#' DSS File Conversion
+#'
+#' Convert between DSS file versions.
+#'
+#' @inheritParams dss_squeeze
+#' @param to The destination file path.
+#'
+#' @seealso [dss_version()] [dss_create()]
+#'
+#' @export
+dss_convert = function(file, to) {
+  assert_dss_connected()
+  assert_dss_file(file)
+  on.exit(file$done(), add = TRUE)
+  file$getDataManager()$convertVersion(to)
+  invisible(TRUE)
+}
+
+
+#' Close DSS File
+#'
+#' Close a DSS file. This function is provided for completion of the
+#' interface but is not strictly required as these methods can be
+#' called directly from the file object (i.e., `file$done()`).
+#'
+#' @inheritParams dss_squeeze
+#' @param implicit If `TRUE`, use method `done()` instead of `close()`.
+#'
+#' @seealso [dss_open()]
+#'
+#' @export
+dss_close = function(file, implicit = FALSE) {
+  assert_dss_connected()
+  assert_dss_file(file)
+  if (implicit) {
+    file$done()
+  } else {
+    file$close()
+  }
   invisible(TRUE)
 }
--- tests/testthat/test-file.R
+++ tests/testthat/test-file.R
@@ -1,7 +1,44 @@
 skip_if_no_dss()
 
-test_that("DSS File open works", {
+test_that("DSS file open works", {
   on.exit(conn$close(), add = TRUE)
   conn = dss_open(system.file("extdata/example.dss", package = "dssrip2"))
   expect_s4_class(conn, "jobjRef")
+})
+
+
+test_that("DSS file creation works", {
+  on.exit(conn$close(), add = TRUE)
+  tf1 = tempfile(fileext = ".dss")
+  tf2 = tempfile(fileext = ".dss")
+  tf3 = tempfile(fileext = ".dss")
+  tf4 = tempfile(fileext = ".dss")
+  tf5 = tempfile(fileext = ".dss")
+  on.exit(unlink(c(tf1, tf2, tf3, tf4, tf5)), add = TRUE)
+
+  f1 = dss_create(tf1)
+  f2 = dss_create(tf2, version = 6L)
+  f3 = dss_create(tf3, version = 7L)
+
+  expect_s4_class(f1, "jobjRef")
+  expect_s4_class(f2, "jobjRef")
+  expect_identical(dss_version(f1), 7L)
+  expect_identical(dss_version(f2), 6L)
+  expect_identical(dss_version(f3), 7L)
+  expect_error(dss_create(tf, version = 5L))
+
+  dss_convert(f1, tf4)
+  dss_convert(f2, tf5)
+  f4 = dss_open(tf4)
+  f5 = dss_open(tf5)
+
+  expect_identical(dss_version(f4), 6L)
+  expect_identical(dss_version(f5), 7L)
+
+  dss_close(f1)
+  dss_close(f2)
+  dss_close(f3)
+  dss_close(f4)
+  dss_close(f5)
+
 })
