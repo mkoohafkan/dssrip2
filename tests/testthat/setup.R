56520c81a843a04584b01123c47cd65668c65e47
Michael Koohafkan <michael.koohafkan@gmail.com>
2023-12-18 17:36:02 -0800

    make tests quiet

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -5,11 +5,12 @@ skip_if_no_dss = function() {
       # look for .dssrip2 folder in temp directory
       dss_home_ci = Sys.getenv("DSS_HOME_CI")
       if (nzchar(dss_home_ci)) {
-        dss_connect(dss_home_ci, monolith = TRUE)
+        dss_connect(dss_home_ci, monolith = TRUE,
+          message_level = 1L, quietly = TRUE)
         return(invisible(TRUE))
       }
   } else {
-    if (dss_require()) {
+    if (dss_require(message_level = 1L, quietly = TRUE)) {
       return(invisible(TRUE))
     }
   }
